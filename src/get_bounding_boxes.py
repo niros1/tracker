653cc3558b3aff2ba84c18f96ba0198fd76649c6
nir oren <nir.o@brainvivo.com>
2024-03-19 19:53:39 +0200

    Add get_bounding_boxes.py script for drawing rectangles on an image

---
+++ src/get_bounding_boxes.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+
+
+image_name = "frame9"
+img_path = f"output/frames/{image_name}.jpg"
+# zoomed_img_path = f"/home/ubuntu/tracker/tracker/output/frames/{image_name}_zoom.jpg"
+# Load an image
+image = cv2.imread(img_path)
+image_copy = image.copy()
+
+# Initialize the list to store rectangle points
+
+rect_endpoint = []
+
+rect_endpoint_tmp = []
+
+
+# Define the event function
+def draw_rectangle(event, x, y, flags, param):
+    global rect_endpoint_tmp, rect_endpoint
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(f"Start point: {x}, {y}")
+        rect_endpoint_tmp[:] = [x, y]
+        start_point = (x, y)
+    elif event == cv2.EVENT_LBUTTONUP:
+        end_point = (x, y)
+        print(f"End point: {x}, {y}")
+        rect_endpoint_tmp += [x, y]
+        cv2.rectangle(
+            image,
+            (rect_endpoint_tmp[0], rect_endpoint_tmp[1]),
+            (rect_endpoint_tmp[2], rect_endpoint_tmp[3]),
+            (255, 255, 255),
+            2,
+        )
+        rect_endpoint += rect_endpoint_tmp
+        cv2.imshow("image", image)
+
+
+# Create a window and assign the callback
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", draw_rectangle)
+
+while True:
+    cv2.imshow("image", image)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+
+# # Print the coordinates of the rectangles
+# for rect in rect_endpoint:
+#     print(f"Start point: {rect}, end point: {rect}")
+print(rect_endpoint)
+# x_points = rect_endpoint[::4]
+# y_points = rect_endpoint[1::4]
+lenght = len(rect_endpoint) / 4
+for i in range(int(len(rect_endpoint) / 4)):
+    idx = i * 4
+    print(
+        f"Start point: {rect_endpoint[idx]} {rect_endpoint[idx+1]}, END POINT {rect_endpoint[idx+2]} {rect_endpoint[idx+3]}"
+    )
